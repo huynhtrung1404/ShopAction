189b12aec80f2fd727cbb45a499eac9d66459f0e
huynhtrung1404 <huynh.trung140495@outlook.com>
2020-09-16 00:46:13 +0700

    create extention for password check

---
+++ src/ShopAction.App/wwwroot/js/passwordExtention.js
@@ -0,0 +1,32 @@
+﻿export class PasswordCheck {
+  static hasMiniumLength(password, minLength) {
+    return password.length >= minLength;
+  }
+  static hasMiniumUniqueChars(password, minUniqueChar) {
+    let unique = password.filter(onlyUnique);
+    return unique >= minUniqueChar;
+  }
+
+  static hasDigit(password) {
+    let format = /\d/;
+    return format.test(password);
+  }
+
+  static hasSpecialChar(password) {
+    let format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return format.test(password);
+  }
+  static hasAnyUpperCase(password) {
+    let format = /[A-Z]/;
+    return format.test(password);
+  }
+  static hasAnyLowelCase(password) {
+    let format = /[a-z]/;
+    return format.test(password)
+  }
+
+  onlyUnique(value,index,self) {
+    return self.indexOf(value) === index
+  }
+
+}
