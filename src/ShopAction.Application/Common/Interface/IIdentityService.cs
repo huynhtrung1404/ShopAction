752db123cd2d992b720cab643ccb5bfcb0329c52
huynhtrung1404 <huynh.trung140495@outlook.com>
2020-11-08 22:23:09 +0700

    Continue with new structure and start with vuejs

---
+++ src/ShopAction.Application/Common/Interface/IIdentityService.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+using ShopAction.Application.Models;
+
+namespace ShopAction.Application.IIdentityService
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+        Task<(Result Result, string UserId)> CreateUserAsync(string userName, string password);
+        Task<Result> DeleteUserAsync(string userId);
+        
+    }
+}
