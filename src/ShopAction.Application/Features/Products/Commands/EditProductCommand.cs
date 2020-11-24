d32031c9e2081dfef68aa9e8b568ab764613a9c1
huynhtrung1404 <huynh.trung140495@outlook.com>
2020-11-25 00:21:17 +0700

    updated code

---
+++ src/ShopAction.Application/Features/Products/Commands/EditProductCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopAction.Application.Features.Products.Commands
+{
+    public class EditProductCommand : IRequest<bool>
+    {
+        public Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public int Stock { get; set; }
+    }
+
+    public class EditProductCommandHandler :IRequestHandler<EditProductCommand, bool>
+    {
+
+    }
+}
