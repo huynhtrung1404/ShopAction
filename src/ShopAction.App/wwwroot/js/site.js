8265c298f77f30ceb9e4f134ed113bfa01ec3560
huynhtrung1404 <huynh.trung140495@outlook.com>
2020-11-28 23:51:03 +0700

    remu rendudant code

--- src/ShopAction.App/wwwroot/js/site.js
+++
@@ -1,7 +0,0 @@
-﻿import  PasswordCheck  from "./passwordExtention.js";
-
-$(document).ready(() => {
-  $('.password-rule').keyup(() => {
-      $('.show-error').text(PasswordCheck.passwordStrength($('.password-rule').val()))
-  })
-})
